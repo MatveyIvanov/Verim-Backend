512d938a751d37c2c01caa1e3328246865c95294
MatveyIvanov <cool.matvey250301@gmail.com>
2023-09-04 02:25:15 +0300

    Добавил базовый конфиг бд

---
+++ auth/config/db.py
@@ -0,0 +1,23 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config.app import get_fastapi_app
+
+
+app = get_fastapi_app()
+
+
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_NAME = os.environ.get('DB_NAME')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
